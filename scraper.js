e70f22ced094042eaf8bd422986a3f4ce281302b
Andy <sstevie17@proton.me>
2026-01-25 20:23:32 -0500

    commit

--- scraper.js
+++ scraper.js
@@ -1,11 +1,9 @@
-const puppeteer = require('puppeteer-core');
-const chromium = require('@sparticuz/chromium');
+const chromium = require('chrome-aws-lambda');
 
 async function getBrowser() {
-  const executablePath = await chromium.executablePath();
-  return await puppeteer.launch({
+  return await chromium.puppeteer.launch({
     args: chromium.args,
-    executablePath: executablePath,
+    executablePath: await chromium.executablePath,
     headless: chromium.headless,
   });
 }
